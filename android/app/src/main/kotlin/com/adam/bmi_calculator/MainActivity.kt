b4aba87d8088c03a8362cf9be10419ac6649b48a
CsAdi <oradam86@gmail.com>
2022-07-21 16:33:59 +0200

    init commit

---
+++ android/app/src/main/kotlin/com/adam/bmi_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.adam.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:bmi_calculator/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
