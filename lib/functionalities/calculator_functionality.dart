d1c536a0d5aeee55f1c6c8541da6720b516556e3
CsAdi <oradam86@gmail.com>
2022-07-27 12:26:50 +0200

    New Files && Create File Structure

---
+++ lib/functionalities/calculator_functionality.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CalculatorFunctionality {
+  const CalculatorFunctionality(
+      {Key? key, required this.height, required this.weight});
+
+  final int height;
+  final int weight;
+}
